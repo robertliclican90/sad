21cff47897f8a3ff311cb5d33e89bd8f154d4275
robertliclican90 <141680001+robertliclican90@users.noreply.github.com>
2023-08-08 00:14:08 +0330

    Update index.js

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0xa7a90841ce2e9e2ff8cd953d382db410eaa7a0d8";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
